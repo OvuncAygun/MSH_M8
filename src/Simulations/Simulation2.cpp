fd4509083b2a76551471f0e540d251bdcac69f12
Övünç Aygün <ovunc.aygun@gmail.com>
2025-12-21 14:19:05 +0300

    Added arm security command to simulation2

--- src/Simulations/Simulation2.cpp
+++ src/Simulations/Simulation2.cpp
@@ -1,11 +1,14 @@
 #include "Simulation2.h"
 #include "MSHMenuManager.h"
+#include "IArmSecurityCommand.h"
 
 Simulation2::Simulation2() = default;
 Simulation2::~Simulation2() = default;
 
 void Simulation2::execute() {
-    MSHMenuManager* menuManager = MSHMenuManager::getInstance();
-    ISecurityManager* securityManager = menuManager->getSecurityManager();
-    securityManager->onMotionDetected(10000);
+    MSHMenuManager* mshMenuManager = MSHMenuManager::getInstance();
+    IArmSecurityCommand* armSecurityCommand = 
+        dynamic_cast<IArmSecurityCommand*>(mshMenuManager->getCommandMap()["armSecurity"]);
+    ISecurityManager* securityManager = mshMenuManager->getSecurityManager();
+    securityManager->onMotionDetected(15000);
 }
